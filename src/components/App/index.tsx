54948b9b902e062eb5bd0ca17f575b053d69054c
Dan Page <dan.page91@gmail.com>
2019-07-15 00:56:24 +0100

    build(config): initial set up of packages

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './styles';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+      <S.Title>Pantry</S.Title>
+      </header>
+    </div>
+  );
+}
+
+export default App;
