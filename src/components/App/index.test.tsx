3495e3b457bd37663be7cf326a26f4d3a53635b0
Dan Page <dan.page91@gmail.com>
2019-07-19 15:03:32 +0100

    refactor(repo): complete rewrite without typescript

--- src/components/App/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from '.';
-
-describe('App component', (): void => {
-  it('should render', (): void => {
-    const { container } = render(<App />);
-    expect(container.firstChild).toMatchSnapshot();
-  });
-});
