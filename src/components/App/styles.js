5a0e5f26dd36642dcf827446acee9c7613a9c053
Dan Page <dan.page91@gmail.com>
2019-07-21 00:16:18 +0100

    fix(styles): styled components display name in dev tools

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components/macro';
 import { normalize } from 'polished';
 
 export const GlobalStyle = createGlobalStyle`
