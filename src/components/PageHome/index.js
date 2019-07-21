b90d99666ee78e9362fe9b2a9bcf1e1f157259c3
Dan Page <dan.page91@gmail.com>
2019-07-21 15:12:40 +0100

    feat(dropdown): add dropdown library for food form

--- src/components/PageHome/index.js
+++ src/components/PageHome/index.js
@@ -1,15 +1,24 @@
 import React, { useContext } from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import dateFns from 'date-fns';
+import Select from 'react-select';
 import { FirebaseContext } from '../ProviderFirebase';
 import Grid from '../Grid';
 import * as S from './styles';
 
+const categoryOptions = [
+    { value: 'meat', label: 'Meat' },
+    { value: 'fish', label: 'Fish' },
+    { value: 'vegetables', label: 'Vegetables' }
+];
+
 const PageHome = () => {
     const { addDoc, value } = useContext(FirebaseContext);
 
+    // need to tidy this up
     const formattedData =
         value &&
+        value.fridge &&
         value.fridge.map((item) => ({
             ...item,
             expires: dateFns.format(item.expires.toDate(), 'MM/DD/YYYY')
@@ -17,20 +26,44 @@ const PageHome = () => {
 
     return (
         <S.Wrapper>
+            {/* need to tidy this up */}
             {formattedData && <Grid data={formattedData} />}
 
             <Formik
                 initialValues={{ category: '', expires: new Date(), name: '', servings: '' }}
+                validate={(values) => {
+                    const errors = {};
+                    if (!values.category) {
+                        errors.category = 'Required';
+                    }
+                    if (!values.name) {
+                        errors.name = 'Required';
+                    }
+                    if (!values.servings) {
+                        errors.servings = 'Required';
+                    }
+                    return errors;
+                }}
                 onSubmit={(values, actions) => {
-                    const immutable = { ...value, fridge: [...value.fridge, values] };
+                    // need to tidy this up
+                    const fridge = [...value.fridge, values].map((item) => ({
+                        ...item,
+                        name: item.name.toLowerCase()
+                    }));
+                    const immutable = { ...value, fridge };
 
                     addDoc(immutable);
                     actions.setSubmitting(false);
                     actions.resetForm();
                 }}
-                render={({ isSubmitting }) => (
+                render={({ isSubmitting, setFieldValue, values }) => (
                     <Form>
-                        <Field type="text" name="category" placeholder="category" />
+                        <Select
+                            name="category"
+                            options={categoryOptions}
+                            onChange={(item) => setFieldValue('category', item)}
+                            value={values.category}
+                        />
                         <ErrorMessage name="category" component="div" />
 
                         <Field type="text" name="name" placeholder="name" />
