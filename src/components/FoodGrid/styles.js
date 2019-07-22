11b5e0aa6c72c0085509e925c44bba1ea8151dde
Dan Page <dan.page91@gmail.com>
2019-07-23 00:24:56 +0100

    feat(food grid): dynamically create headings

--- src/components/FoodGrid/styles.js
+++ src/components/FoodGrid/styles.js
@@ -10,7 +10,13 @@ export const Wrapper = styled.ul`
     width: max-content;
 `;
 
-export const Heading = styled.h5`
+export const Heading = styled.h2`
     font-size: 18px;
     margin: 0 0 1rem;
+`;
+
+export const Category = styled.h3`
+    font-size: 16px;
+    margin: 0 0 1rem;
+    grid-column: 1 / 5;
 `;
