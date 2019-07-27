de5a58849f745d3a8a8dcd60d09c578f92b174e4
Dan Page <dan.page91@gmail.com>
2019-07-27 18:07:53 +0100

    feat(input): built an input component with labels and errors

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import { string } from 'prop-types';
 import * as S from './styles';
 
-const Input = ({ testId }) => <S.Input data-testid={testId} type="text" />;
+const Input = ({ error, label, testId }) => {
+    return (
+        <S.Wrapper>
+            {label && <S.Label htmlFor={testId}>{label}</S.Label>}
+
+            <S.Input id={testId} data-testid={testId} type="text" />
+
+            {error && <S.Error>{error}</S.Error>}
+        </S.Wrapper>
+    );
+};
 
 Input.propTypes = {
+    error: string,
+    label: string,
     testId: string.isRequired
+};
+
+Input.defaultProps = {
+    error: '',
+    label: ''
 };
 
 export default Input;
--- src/components/Input/index.test.js
+++ src/components/Input/index.test.js
@@ -10,4 +10,18 @@ describe('Input component', () => {
         const { container } = render(<Input {...props} />);
         expect(container.firstChild).toMatchSnapshot();
     });
+
+    it('should show error', () => {
+        const error = 'Required';
+        const { getByText } = render(<Input {...props} error={error} />);
+        const errorMessage = getByText(error);
+
+        expect(errorMessage).toHaveStyleRule('color', 'red');
+    });
+
+    it('should show label', () => {
+        const label = 'Name';
+        const { getByText } = render(<Input {...props} label={label} />);
+        getByText(label);
+    });
 });
