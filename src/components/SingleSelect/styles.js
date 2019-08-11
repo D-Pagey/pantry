abedcb443e03d2cd034934c069e2aed7d6193ba5
Dan Page <dan.page91@gmail.com>
2019-08-11 19:20:43 +0100

    feat(add food form): add single select serving size

--- src/components/SingleSelect/styles.js
+++ src/components/SingleSelect/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    margin: ${({ margin }) => margin};
+    margin: ${({ margin }) => margin || '0 0 1rem'};
 `;
 
 export const Label = styled.label`
