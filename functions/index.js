93d9255ca96de6c88447cd4f8186412cf12ea165
Dan Page <dan.page91@gmail.com>
2019-12-09 02:22:00 +0000

    feat(function): add func to create user obj in firestore

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
@@ -9,6 +8,14 @@ admin.initializeApp();
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+exports.userCreated = functions.auth.user().onCreate(async (user) => {
+    console.log('Creating user: ', user.displayName)
+
+    try {
+        await admin.firestore().collection('users').doc(user.uid).set({ id: user.uid, email: user.email, name: user.displayName });
+        console.log('User created:', user.displayName)
+    } catch (error) {
+        console.log('Error creating user: ', error)
+    }
+
+  });
