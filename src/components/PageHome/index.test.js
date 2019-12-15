f6d1910ee66c987b57a532d6824b532f078e68b5
Dan Page <dan.page91@gmail.com>
2019-12-15 13:05:44 +0000

    test(home page): add test to only render content if authed

--- src/components/PageHome/index.test.js
+++ src/components/PageHome/index.test.js
@@ -5,9 +5,18 @@ const props = {
     categoryCounts: [{ category: 'Meat', count: 1 }]
 };
 
+const context = {
+    isAuthed: false
+};
+
 describe('PageHome component', () => {
     it('should render', () => {
-        const { container } = render(<PageHome {...props} />);
+        const { container } = render(<PageHome {...props} />, context);
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('should render category list if authed', () => {
+        const { getByTestId } = render(<PageHome {...props} />, { isAuthed: true });
+        getByTestId('categoryList');
     });
 });
