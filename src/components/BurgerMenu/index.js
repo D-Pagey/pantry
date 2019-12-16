7095ce3896eb4f8dc89dbbc4727d3ee8c5bc37ea
Dan Page <dan.page91@gmail.com>
2019-12-16 21:12:41 +0000

    fix(menu): conditional nav links

--- src/components/BurgerMenu/index.js
+++ src/components/BurgerMenu/index.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
+import { AuthContext } from '../ProviderAuth';
 import * as S from './styles';
 
 const BurgerMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const { isAuthed } = useContext(AuthContext);
 
     const handleStateChange = (state) => setIsOpen(state.isOpen);
     const closeMenu = () => setIsOpen(false);
@@ -19,15 +21,21 @@ const BurgerMenu = () => {
             <S.NavLink to="/" onClick={closeMenu}>
                 Home
             </S.NavLink>
-            <S.NavLink to="/add" onClick={closeMenu}>
-                Add Food
-            </S.NavLink>
-            <S.NavLink to="/profile" onClick={closeMenu}>
-                Profile
-            </S.NavLink>
-            <S.NavLink to="/sign-in" onClick={closeMenu}>
-                Sign In
-            </S.NavLink>
+            {isAuthed ? (
+                <>
+                    <S.NavLink to="/add" onClick={closeMenu}>
+                        Add Food
+                    </S.NavLink>
+                    <S.NavLink to="/profile" onClick={closeMenu}>
+                        Profile
+                    </S.NavLink>
+                    <S.Button onClick={closeMenu}>Sign Out</S.Button>
+                </>
+            ) : (
+                <S.NavLink to="/sign-in" onClick={closeMenu}>
+                    Sign In
+                </S.NavLink>
+            )}
         </Menu>
     );
 };
--- src/components/BurgerMenu/styles.js
+++ src/components/BurgerMenu/styles.js
@@ -4,6 +4,22 @@ import { NavLink as Link } from 'react-router-dom';
 export const NavLink = styled(Link)`
     color: white;
     text-decoration: none;
+    margin: 0 0 1rem;
+
+    &:hover {
+        color: orange;
+    }
+`;
+
+export const Button = styled.button.attrs({
+    type: 'button'
+})`
+    background-color: rgb(55, 58, 71);
+    border: none;
+    color: white;
+    display: inline-flex;
+    padding: 0;
+    text-decoration: none;
 
     &:hover {
         color: orange;
@@ -11,6 +27,7 @@ export const NavLink = styled(Link)`
 `;
 
 export const MenuStyles = {
+    /* Position and sizing of burger button */
     bmBurgerButton: {
         position: 'fixed',
         width: '36px',
@@ -18,38 +35,61 @@ export const MenuStyles = {
         right: '36px',
         top: '36px'
     },
+
+    /* Color/shape of burger icon bars */
     bmBurgerBars: {
         background: '#373a47'
     },
+
+    /* Color/shape of burger icon bars on hover*/
     bmBurgerBarsHover: {
         background: '#a90000'
     },
+
+    /* Position and sizing of clickable cross button */
     bmCrossButton: {
         height: '24px',
         width: '24px'
     },
+
+    /* Color/shape of close button cross */
     bmCross: {
         background: '#bdc3c7'
     },
+
+    /*
+        Sidebar wrapper styles
+        Note: Beware of modifying this element as it can break the animations - you should not need to touch it in most cases
+    */
     bmMenuWrap: {
         position: 'fixed',
         height: '100%'
     },
+
+    /* General sidebar styles */
     bmMenu: {
         background: '#373a47',
         padding: '2.5em 1.5em 0',
         fontSize: '1.15em'
     },
+
+    /* Morph shape necessary with bubble or elastic */
     bmMorphShape: {
         fill: '#373a47'
     },
+
+    /* Wrapper for item list */
     bmItemList: {
         color: '#b8b7ad',
-        padding: '0.8em'
+        padding: '0.8em',
+        display: 'flex',
+        flexDirection: 'column'
     },
-    bmItem: {
-        margin: '0 0 1rem'
-    },
+
+    /* Individual item */
+    bmItem: {},
+
+    /* Styling of overlay */
     bmOverlay: {
         background: 'rgba(0, 0, 0, 0.3)'
     }
