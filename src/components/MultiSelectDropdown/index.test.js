ca4b486e2c6a7e1dedf9dab68fdef64738e02d8e
Dan Page <dan.page91@gmail.com>
2020-04-02 09:26:38 +0100

    test(multi select): mock react-select to test onChange handler

--- src/components/MultiSelectDropdown/index.test.js
+++ src/components/MultiSelectDropdown/index.test.js
@@ -1,8 +1,30 @@
+/* eslint-disable no-shadow */
 import React from 'react';
-import { MultiSelectDropdown} from '.';
+import userEvent from '@testing-library/user-event';
+import { MultiSelectDropdown } from '.';
+
+jest.mock('react-select/creatable', () => ({ options, value, onChange }) => {
+    const handleChange = (event) => {
+        const option = options.find((option) => option.value === event.currentTarget.value);
+        onChange(option);
+    };
+
+    return (
+        <select data-testid="select" value={value} onChange={handleChange}>
+            {options.map(({ label, value }) => (
+                <option key={value} value={value}>
+                    {label}
+                </option>
+            ))}
+        </select>
+    );
+});
 
 const props = {
-    options: [{ label: 'Meat', value: 'meat' }],
+    options: [
+        { label: 'Meat', value: 'meat' },
+        { label: 'Fish', value: 'fish' }
+    ],
     setValues: () => {},
     values: []
 };
@@ -17,11 +39,21 @@ describe('MultiSelectDropdown component', () => {
         const label = 'Which food categories is it?';
         const { getByText } = render(<MultiSelectDropdown {...props} label={label} />);
         getByText(label);
-      });
-    
-      it('should render an error', () => {
+    });
+
+    it('should render an error', () => {
         const error = 'Required';
         const { getByText } = render(<MultiSelectDropdown {...props} error={error} />);
         getByText(error);
-      });
+    });
+
+    it('should call setValues when changed', () => {
+        const setValues = jest.fn();
+        const option = { label: 'Fish', value: 'fish' };
+        const { getByTestId } = render(<MultiSelectDropdown {...props} setValues={setValues} />);
+
+        userEvent.selectOptions(getByTestId('select'), option.value);
+
+        expect(setValues).toHaveBeenCalledWith(option);
+    });
 });
