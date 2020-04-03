d53ce540810946841baeafc3b0e23fffb26f7116
Dan Page <dan.page91@gmail.com>
2020-04-03 23:14:00 +0100

    fix(multi-select): wIP on new option logic

--- src/components/MultiSelectDropdown/index.tsx
+++ src/components/MultiSelectDropdown/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CreatableSelect from 'react-select/creatable';
+import { ValueType } from 'react-select';
 import { FormLabel } from '../FormLabel';
 import { FormError } from '../FormError';
 import * as S from './styles';
@@ -21,7 +22,12 @@ type DropdownTypes = {
 };
 
 export const MultiSelectDropdown = ({ error, label, options, setValues, value }: DropdownTypes): JSX.Element => {
-    const handleChange = (newValue: any): void => {
+    const handleChange = (newValue: any, actionMeta: any): void => {
+        console.log(actionMeta);
+        if (actionMeta.action === 'create-option') {
+            console.log({ newValue: newValue[newValue.length - 1] });
+        }
+
         return setValues(newValue);
     };
 
