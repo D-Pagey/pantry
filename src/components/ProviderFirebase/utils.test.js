1ca44e26f15bba784030ef99ddf070fcd426454b
Dan Page <dan.page91@gmail.com>
2020-04-05 16:57:04 +0100

    feat(update categories): add new function

--- src/components/ProviderFirebase/utils.test.js
+++ src/components/ProviderFirebase/utils.test.js
@@ -1,66 +1,109 @@
 import { addDays } from 'date-fns';
-import { calculateExpiringSoon, indexOfLabel } from './utils';
+import { calculateExpiringSoon, indexOfLabel, updateCategories } from './utils';
 
 describe('calculateExpiringSoon function', () => {
-  const food = [
-    {
-      category: { label: 'meat', color: 'red' },
-      expires: addDays(new Date(), 1),
-      id: '1245',
-      name: 'chicken',
-      servings: 2,
-    },
-    {
-      category: { label: 'fish', color: 'blue' },
-      expires: addDays(new Date(), 5),
-      id: '5678',
-      name: 'salmon',
-      servings: 1,
-    },
-  ];
+    const food = [
+        {
+            categories: ['111', '222'],
+            expires: addDays(new Date(), 1),
+            id: '1245',
+            name: 'chicken',
+            servings: 2
+        },
+        {
+            categories: ['111', '222'],
+            expires: addDays(new Date(), 5),
+            id: '5678',
+            name: 'salmon',
+            servings: 1
+        }
+    ];
 
-  it('should return an array', () => {
-    expect(Array.isArray(calculateExpiringSoon(food))).toBe(true);
-  });
+    it('should return an array', () => {
+        expect(Array.isArray(calculateExpiringSoon(food))).toBe(true);
+    });
 
-  it('should add an isExpiringSoon property to each food item', () => {
-    expect(calculateExpiringSoon(food)).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          isExpiringSoon: expect.any(Boolean),
-        }),
-      ]),
-    );
-  });
+    it('should add an isExpiringSoon property to each food item', () => {
+        expect(calculateExpiringSoon(food)).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    isExpiringSoon: expect.any(Boolean)
+                })
+            ])
+        );
+    });
 
-  it('should return correct values for isExpiringSoon', () => {
-    const result = calculateExpiringSoon(food);
+    it('should return correct values for isExpiringSoon', () => {
+        const result = calculateExpiringSoon(food);
 
-    expect(result[0]).toEqual(
-      expect.objectContaining({
-        isExpiringSoon: true,
-      }),
-    );
-    expect(result[1]).toEqual(
-      expect.objectContaining({
-        isExpiringSoon: false,
-      }),
-    );
-  });
+        expect(result[0]).toEqual(
+            expect.objectContaining({
+                isExpiringSoon: true
+            })
+        );
+        expect(result[1]).toEqual(
+            expect.objectContaining({
+                isExpiringSoon: false
+            })
+        );
+    });
 });
 
+// TODO: Delete this, is it needed with new categories?
 describe('indexOfLabel function', () => {
-  it('should return a number', () => {
-    expect(typeof indexOfLabel(['fish', 'meat', 'chocolate'], 'fish')).toBe('number');
-  });
+    it('should return a number', () => {
+        expect(typeof indexOfLabel(['fish', 'meat', 'chocolate'], 'fish')).toBe('number');
+    });
 
-  it('should return the index of a label', () => {
-    const array = ['fish', 'meat', 'vegetables', 'chocolate'];
+    it('should return the index of a label', () => {
+        const array = ['fish', 'meat', 'vegetables', 'chocolate'];
 
-    expect(indexOfLabel(array, 'fish')).toBe(0);
-    expect(indexOfLabel(array, 'meat')).toBe(1);
-    expect(indexOfLabel(array, 'vegetables')).toBe(2);
-    expect(indexOfLabel(array, 'dairy')).toBe(-1);
-    expect(indexOfLabel(array, 'chocolate')).toBe(3);
-  });
+        expect(indexOfLabel(array, 'fish')).toBe(0);
+        expect(indexOfLabel(array, 'meat')).toBe(1);
+        expect(indexOfLabel(array, 'vegetables')).toBe(2);
+        expect(indexOfLabel(array, 'dairy')).toBe(-1);
+        expect(indexOfLabel(array, 'chocolate')).toBe(3);
+    });
+});
+
+describe.only('updateCategories function', () => {
+    const allCategories = [
+        {
+            colour: 'red',
+            count: 0,
+            id: '111',
+            label: 'Meat',
+            value: 'meat'
+        },
+        {
+            colour: 'blue',
+            count: 0,
+            id: '222',
+            label: 'Fish',
+            value: 'fish'
+        },
+        {
+            colour: 'green',
+            count: 0,
+            id: '333',
+            label: 'Vegetables',
+            value: 'vegetables'
+        }
+    ];
+
+    const itemCategoryIds = [allCategories[1].id];
+
+    it('should return an array', () => {
+        expect(Array.isArray(updateCategories(allCategories, itemCategoryIds))).toBe(true);
+    });
+
+    it.only('should increase the count of a category when adding a category', () => {
+        const result = updateCategories(allCategories, itemCategoryIds);
+
+        expect(result).toStrictEqual([
+            allCategories[0],
+            { ...allCategories[1], count: allCategories[1].count + 1 },
+            allCategories[2]
+        ]);
+    });
 });
--- src/components/ProviderFirebase/utils.ts
+++ src/components/ProviderFirebase/utils.ts
@@ -1,10 +1,8 @@
 import { differenceInDays } from 'date-fns';
+import { CategoryType } from '../../types';
 
 interface FoodType {
-  category: {
-    label: string;
-    color: string;
-  };
+  categories: string[];
   expires: Date;
   id: string;
   name: string;
@@ -20,6 +18,7 @@ export const indexOfLabel = (arrayOfLabels: string[], label: string): number => 
   return acc;
 }, -1);
 
+// TODO: Delete this
 export const calculateExpiringSoon = (food: FoodType[]): FoodWithExpiringType[] => food.map((item) => {
   const difference = differenceInDays(item.expires, new Date());
 
@@ -28,3 +27,16 @@ export const calculateExpiringSoon = (food: FoodType[]): FoodWithExpiringType[] 
     isExpiringSoon: difference <= 2,
   };
 });
+
+export const updateCategories = (allCategories: CategoryType[], itemCategoryIds: string[]): CategoryType[] => {
+  return allCategories.reduce((acc, curr) => {
+    // when the selected categories are in the total categories
+    // increment the count
+    if (itemCategoryIds.includes(curr.id)) {
+        const incrementedCount = {...curr, count: curr.count + 1};
+        return [...acc, incrementedCount];
+      }
+
+      return [...acc, curr];
+  }, [] as CategoryType[]);
+};
