e91d620f1e7f425472e7184061c73aeadac415e8
Dan Page <dan.page91@gmail.com>
2020-05-30 00:29:59 +0100

    fix(category list): sort categories alphabetically

--- src/components/CategoryList/index.tsx
+++ src/components/CategoryList/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import arraySort from 'array-sort';
 import { FirebaseContext } from '../ProviderFirebase';
 import { CategoryCard } from '../CategoryCard';
 import * as S from './styles';
@@ -12,10 +13,12 @@ type ItemType = {
 export const CategoryList = (): JSX.Element => {
     const { categories, fridge } = useContext(FirebaseContext);
 
+    const alphabetical = arraySort([...categories], 'name');
+
     return (
         <S.Wrapper data-testid="categoryList">
             <CategoryCard name="all" colour="orange" quantity={fridge.length} />
-            {categories.map((item: ItemType) => {
+            {alphabetical.map((item: ItemType) => {
                 if (item.count > 0) {
                     return <CategoryCard name={item.name} colour={item.colour} quantity={item.count} key={item.name} />;
                 }
