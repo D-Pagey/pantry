bfc64e9b24a37044645d5b90ae5ca63c4c529e7a
Dan Page <dan.page91@gmail.com>
2020-06-07 17:22:32 +0100

    fix(loading): change colour

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropagateLoader from 'react-spinners/PropagateLoader';
+import { colours } from '../../tokens';
 import * as S from './styles';
 
 type LoadingTypes = {
@@ -8,6 +9,6 @@ type LoadingTypes = {
 
 export const Loading = ({ isLoading }: LoadingTypes): JSX.Element => (
     <S.Wrapper data-testid="loading">
-        <PropagateLoader color="blue" loading={isLoading} />
+        <PropagateLoader color={colours.darkGreen100} loading={isLoading} />
     </S.Wrapper>
 );
