00421d4098c7d2b2e6c9cf97bc57cacb7e304a58
Dan Page <dan.page91@gmail.com>
2020-07-01 19:47:41 +0100

    test(donut): add test for expired donut

--- src/components/Donut/index.test.tsx
+++ src/components/Donut/index.test.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { addDays } from 'date-fns';
+
 import { render } from '../../test-utils';
 import { Donut } from '.';
 
@@ -10,5 +12,10 @@ describe('Donut component', () => {
     it('should render', () => {
         const { container } = render(<Donut {...props} />);
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('should render expired text when passed expired date', () => {
+        const { getByText } = render(<Donut {...props} date={addDays(new Date(), -1)} />);
+        getByText('Expired');
     });
 });
