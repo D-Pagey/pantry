2339f282047a73f8bfbf21155fbf842c6aaa90db
Dan Page <dan.page91@gmail.com>
2020-07-19 18:32:46 +0100

    fix(error boundary): add basic layout and button

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import { Button } from '../Button';
+import { Layout } from '../Layout';
+
 export class ErrorBoundary extends React.Component {
-    constructor(props) {
+    constructor(props: any) {
         super(props);
         this.state = { hasError: false };
     }
@@ -11,18 +14,27 @@ export class ErrorBoundary extends React.Component {
         return { hasError: true };
     }
 
-    componentDidCatch(error, errorInfo) {
+    componentDidCatch(error: any, errorInfo: any) {
         // You can also log the error to an error reporting service
         console.error(error, errorInfo);
     }
 
     render() {
+        // @ts-ignore
         const { hasError } = this.state;
         const { children } = this.props;
 
         if (hasError) {
             // You can render any custom fallback UI
-            return <h1>Something went wrong.</h1>;
+            return (
+                <Layout title="Uh Oh!">
+                    <h1>Something went wrong.</h1>
+
+                    <a href="/">
+                        <Button>Return Home</Button>
+                    </a>
+                </Layout>
+            );
         }
 
         return children;
