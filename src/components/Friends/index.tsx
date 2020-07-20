41247f54c525ed0426b0a24b297f6368ec407785
Dan Page <dan.page91@gmail.com>
2020-07-20 20:17:00 +0100

    build(types): update all and linter

--- src/components/Friends/index.tsx
+++ src/components/Friends/index.tsx
@@ -10,7 +10,7 @@ type FriendsProps = {
 export const Friends: FC<FriendsProps> = ({ friends }) => (
     <S.List>
         {friends.map((friend) => (
-            <S.Item>
+            <S.Item key={friend.uid}>
                 <S.Span>{friend.name}</S.Span>
                 <S.Span>{friend.email}</S.Span>
                 <S.Image src={friend.photo} alt="user" />
