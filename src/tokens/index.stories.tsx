8b24411ce530a224352123f40b0ef12bce2bb9ba
Dan Page <dan.page91@gmail.com>
2020-08-08 00:26:58 +0100

    chore(storybook): add colour palette story

---
+++ src/tokens/index.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { colours } from '.';
+
+export default { title: 'Colours' };
+
+export const all = () => (
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', gridGap: '1rem' }}>
+        {Object.entries(colours).map(([name, color]) => (
+            <div>
+                <p>{name}</p>
+                <div style={{ width: 50, height: 50, backgroundColor: color }} />
+            </div>
+        ))}
+    </div>
+);
