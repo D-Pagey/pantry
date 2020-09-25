c8e9191a854718e8320f6a9d7366d2ff080a3508
Dan Page <dan.page91@gmail.com>
2020-09-25 12:50:29 +0100

    fix(storybook): update FoodCard story

--- src/components/FoodCard/index.stories.tsx
+++ src/components/FoodCard/index.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Batches } from '../../fixtures';
+import { Batches, TenantDan, TenantHeidi, TenantJoe } from '../../fixtures';
 import { FoodCard, FoodCardProps } from '.';
 
 export default { title: 'FoodCard', component: FoodCard };
@@ -11,16 +11,18 @@ Basic.args = {
     handleClick: () => console.log('fired'),
     margin: '1rem 0 0 1rem',
     name: 'Carrots',
+    tenants: [TenantDan, TenantHeidi, TenantJoe]
 };
 
 Basic.argTypes = {
-    batches: { 
-        control: { 
-            type: 'select', 
+    batches: {
+        control: {
+            type: 'select',
             options: { multipleOwners: Batches, singleOwner: [Batches[0]] }
-         },
+        }
         //  defaultValue: Batches
     },
+    tenants: { control: { disable: true } },
     handleClick: { control: { disable: true } },
-    margin: { control: { disable: true } },
+    margin: { control: { disable: true } }
 };
