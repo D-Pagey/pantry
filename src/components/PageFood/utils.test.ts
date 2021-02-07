24d632bae412894d58bf236863c3878014470828
Dan Page <dan.page91@gmail.com>
2021-02-07 12:00:09 +0000

    test(apply-multi-filters): cover in tests

--- src/components/PageFood/utils.test.ts
+++ src/components/PageFood/utils.test.ts
@@ -1,7 +1,8 @@
 import { addDays } from 'date-fns';
 import { TenantAlexa, TenantDan, TenantHeidi, TenantJoe } from '../../fixtures';
 import { FoodType, BatchType, TenantType } from '../../types';
-import { sortByName, sortByOldestExpiryDate, getOwnersButtonText } from './utils';
+import { FilterState } from '../MobileFoodMenu/filterReducer';
+import { applyMultipleFilters, sortByName, sortByOldestExpiryDate, getOwnersButtonText } from './utils';
 
 export const Batch: BatchType = {
     id: '3333333',
@@ -74,5 +75,145 @@ describe('getOwnersButtonText function', () => {
 
         const text = getOwnersButtonText(selectedOwnersIds, tenants);
         expect(text).toBe(expectedText);
+    });
+});
+
+const defaultFilters: FilterState = {
+    selectedOwners: [],
+    showOnlyExpiring: false,
+    sortBy: 'name'
+};
+
+const ExpiringZebra: FoodType = {
+    name: 'zebra',
+    category: 'meat',
+    unit: 'servings',
+    batches: [
+        {
+            expires: new Date(),
+            quantity: 2,
+            id: 'asdadsadsda',
+            ownerId: TenantDan.uid
+        }
+    ]
+};
+
+const FreshApple: FoodType = {
+    name: 'apple',
+    category: 'fruit',
+    unit: 'units',
+    batches: [
+        {
+            expires: addDays(new Date(), 5),
+            quantity: 3,
+            id: 'asdadsadsda',
+            ownerId: TenantHeidi.uid
+        }
+    ]
+};
+
+const AverageCarrot: FoodType = {
+    name: 'carrot',
+    category: 'vegetables',
+    unit: 'units',
+    batches: [
+        {
+            expires: addDays(new Date(), 4),
+            quantity: 5,
+            id: 'asdadsadsda',
+            ownerId: TenantJoe.uid
+        }
+    ]
+};
+
+describe('applyMultipleFilters function', () => {
+    it('should filter out items without batches', () => {
+        const foodWithoutBatches = [FreshApple, ExpiringZebra, { ...AverageCarrot, batches: [] }];
+
+        const filtered = applyMultipleFilters(foodWithoutBatches, defaultFilters);
+
+        expect(filtered.length).toBe(foodWithoutBatches.length - 1);
+    });
+
+    it('should sort by name', () => {
+        const filters: FilterState = {
+            ...defaultFilters,
+            sortBy: 'name'
+        };
+
+        const sortedByName = applyMultipleFilters([ExpiringZebra, FreshApple, AverageCarrot], filters);
+
+        const sortedItemsByName = [FreshApple, AverageCarrot, ExpiringZebra];
+
+        expect(sortedByName).toStrictEqual(sortedItemsByName);
+    });
+
+    it('should sort by date', () => {
+        const filters: FilterState = {
+            ...defaultFilters,
+            sortBy: 'date'
+        };
+
+        const sortedByName = applyMultipleFilters([AverageCarrot, FreshApple, ExpiringZebra], filters);
+
+        const sortedItemsByName = [ExpiringZebra, AverageCarrot, FreshApple];
+
+        expect(sortedByName).toStrictEqual(sortedItemsByName);
+    });
+
+    it('should not filter by owner if no expiring, no selected owners and items with batches', () => {
+        const filters: FilterState = {
+            ...defaultFilters,
+            showOnlyExpiring: false,
+            selectedOwners: []
+        };
+
+        const food = [AverageCarrot, FreshApple, ExpiringZebra];
+
+        const notFiltered = applyMultipleFilters(food, filters);
+
+        expect(notFiltered.length).toBe(food.length);
+    });
+
+    it('should filter by owner if passed selected owners', () => {
+        const filters: FilterState = {
+            ...defaultFilters,
+            showOnlyExpiring: false,
+            selectedOwners: [TenantDan.uid]
+        };
+
+        const food = [AverageCarrot, FreshApple, ExpiringZebra];
+
+        const notFiltered = applyMultipleFilters(food, filters);
+
+        expect(notFiltered.length).toBe(1);
+    });
+
+    it('should not filter by expiring if passed showOnlyExpiring = false', () => {
+        const filters: FilterState = {
+            ...defaultFilters,
+            showOnlyExpiring: false,
+            selectedOwners: []
+        };
+
+        const food = [AverageCarrot, FreshApple, ExpiringZebra];
+
+        const notFiltered = applyMultipleFilters(food, filters);
+
+        expect(notFiltered.length).toBe(food.length);
+    });
+
+    it('should filter by expiring if passed showOnlyExpiring', () => {
+        const filters: FilterState = {
+            ...defaultFilters,
+            showOnlyExpiring: true,
+            selectedOwners: []
+        };
+
+        const food = [AverageCarrot, FreshApple, ExpiringZebra];
+
+        const filtered = applyMultipleFilters(food, filters);
+
+        expect(filtered).toStrictEqual([ExpiringZebra]);
     });
 });
